142a21f8ffb597a6d1b1e33cf9b9afc60dc51b2a
FlorestDev <kirillborisenko2012@gmail.com>
2024-11-25 13:35:12 +0300

    Новый параметр.

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ proxies = {} # прокси, которые используются ботом.
 headers_for_html_requests = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36', 'Accept-Language': 'ru-RU'} # Юзер агент, а также язык, на котором будут возвращаться данные при парсинге любого сайта.
 google_api_key = None # Сюда введите токен от Google APIs для того, чтобы можно было узнать данные о канале, или узнать данные о видео при скачивании. Токен можно получить тут: https://developers.google.com/identity/oauth2/web/guides/get-google-api-clientid (заходить с VPN)
 gigachat_token = '' # токен от GigaChat API (ПАО "СберБанк")
+gigachat_id = '' # ID клиента GigaChat.
