98c11426cf9fdd2534db2d3766d5d5572c225483
FlorestDev <kirillborisenko2012@gmail.com>
2025-11-19 10:20:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,57 @@
-token = '...'
-token_of_payment = '...'
-telegram_channel_id = '-...'
-proxies = {"http":'http://...', 'https':'http://...'}
-chat_id = '...'
-banned_words = ['продам', 'куплю', 'покажу имтимные фото', 'подарите премиум', 'москаль', 'москали', 'русня', 'набираю на работу', 'набираю активных подписчиков', 'накручу активных подписчиков', 'отзывы по ссылке в био', 'покажу свои самые лучшие имтиные фото и видео', '#ищуработу', '#заработок', '#заработак', 'ищем воркеров в тиму']
+token = ""
+token_of_payment = ""
+telegram_channel_id = ""
+webhook_url = ""
+proxies = {
+    "http": "",
+    "https": ""
+}
+
+token_donation_alerts = ""
+donations = []
+chat_id = 0
+
+banned_words = [
+    'продам', 'куплю', 'покажу имтимные фото', 'подарите премиум', 'москаль', 'москали',
+    'русня', 'набираю на работу', 'набираю активных подписчиков', 'накручу активных подписчиков',
+    'отзывы по ссылке в био', 'покажу свои самые лучшие имтиные фото и видео', '#ищуработу',
+    '#заработок', '#заработак', 'ищем воркеров в тиму'
+]
+
 ids_for_people_who_make_a_victorina = []
-headers_for_html_requests = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36', 'Accept-Language': 'ru-RU'}
-google_api_key = '...'
-ideas = ['Как найти отношения?', 'Сделай код на Python', 'Сделай простой сайтик', 'Найди НОК', 'Найди НОД', 'Расскажи интересный факт']
-error_gif = '...'
-give_me_gif = '...'
-under_gif = '...'
-water_sign = '@postbotflorestbot'
-maintenance = {"work":False, "reason":"Технические работы.", "time":"неизвестно"}
-group_id = -1002184607448
-gigachat_id = '...'
-gigachat_token = '...'
-username_mail = '...'
-passwd_mail = '...'
-virustotal_apikey = "..."
-vk_useragent = 'KateMobileAndroid/56 lite-460 (Android 4.4.2; SDK 19; x86; unknown Android SDK built for x86; en)'
-token_for_vk = '...'
+
+headers_for_html_requests = {
+    'User-Agent': '',
+    'Accept-Language': 'ru-RU'
+}
+
+google_api_key = ""
+ideas = [
+    'Как найти отношения?', 'Сделай код на Python', 'Сделай простой сайтик',
+    'Найди НОК', 'Найди НОД', 'Расскажи интересный факт'
+]
+
+error_gif = ""
+give_me_gif = ""
+under_gif = ""
+
+water_sign = "@postbotflorestbot"
+
+maintenance = {
+    "work": False,
+    "reason": "Технические работы.",
+    "time": "неизвестно"
+}
+
+group_id = 0
+gigachat_token = ""
+
+username_mail = ""
+passwd_mail = ""
+
+virustotal_apikey = ""
+useragent_for_vk = ""
+token_for_vk = ""
+whisper_api_token = ""
+
+sambanova_api_key = ""
